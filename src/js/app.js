220c8cd23e9ea2a68a1115cb02b5013c333cc8f2
Voitsikhovska <daravojka505@gmail.com>
2024-02-01 16:24:31 +0200

    create hamburger menu for the adapted version and adapting the first sections to the screen (max width 1200px)

--- src/js/app.js
+++ src/js/app.js
@@ -60,3 +60,13 @@ $(document).ready(function () {
   });
 });
 
+const hamburger = document.querySelector(".hamburger"),
+  menu = document.querySelector(".menu"),
+  closeElem = document.querySelector(".menu_close");
+hamburger.addEventListener("click", () => {
+  menu.classList.add("active");
+});
+closeElem.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+
